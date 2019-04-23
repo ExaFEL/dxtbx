c1d4bd493451a95b4978091ead50a20a275b533c
Markus Gerstel <markus.gerstel@diamond.ac.uk>
2019-04-23 21:59:25 +0100

    add new command: libtbx.precommit
    TL;DR: run 'libtbx.precommit install'
    
    libtbx.precommit installs a fully fledged pre-commit environment
    supporting the full range of possible pre-commit plugins, and
    enables them for all supported repositories.
    
    This environment is separate from the cctbx python environment, and
    installed into build/precommitbx.
    The pre-commit dependencies are installed into said environment via the
    precommitbx python package, which lives inside dxtbx/precommitbx.
    libtbx.precommit takes care of installing and maintaining the git hook.
    The pre-commit environment may not be relocatable, but can be fully
    restored by removing the build/precommitbx directory and re-running the
    installer.

---
+++ command_line/precommitbx.py
@@ -0,0 +1,7 @@
+# LIBTBX_SET_DISPATCHER_NAME libtbx.precommit
+
+from __future__ import absolute_import, division, print_function
+
+import dxtbx.precommitbx.installer
+
+dxtbx.precommitbx.installer.main()
